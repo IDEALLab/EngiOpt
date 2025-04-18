cd52d01e0468e3d29b51978e0ceaab7ec3928946
Matthew Keeler <mkeeler@ethz.ch>
2025-04-18 19:29:59 +0200

    Set up for HPC testing

--- engiopt/run_2d_models.py
+++ engiopt/run_2d_models.py
@@ -6,11 +6,13 @@ import subprocess
 
 # Define your models, problems, and seeds
 models = {
-    "cgan_2d": os.path.join("engiopt/cgan_2d", "cgan_2d.py"),
-    "gan_2d": os.path.join("engiopt/gan_2d", "gan_2d.py"),
-    "cgan_cnn_2d": os.path.join("engiopt/cgan_cnn_2d", "cgan_cnn_2d.py"),
-    "gan_cnn_2d": os.path.join("engiopt/gan_cnn_2d", "gan_cnn_2d.py"),
-    "diffusion_2d_cond": os.path.join("engiopt/diffusion_2d_cond", "diffusion_2d_cond.py"),
+    "cgan_2d": os.path.join(os.environ["HOME"], "projects/EngiOpt/engiopt/cgan_2d", "cgan_2d.py"),
+    "gan_2d": os.path.join(os.environ["HOME"], "projects/EngiOpt/engiopt/gan_2d", "gan_2d.py"),
+    "cgan_cnn_2d": os.path.join(os.environ["HOME"], "projects/EngiOpt/engiopt/cgan_cnn_2d", "cgan_cnn_2d.py"),
+    "gan_cnn_2d": os.path.join(os.environ["HOME"], "projects/EngiOpt/engiopt/gan_cnn_2d", "gan_cnn_2d.py"),
+    "diffusion_2d_cond": os.path.join(
+        os.environ["HOME"], "projects/EngiOpt/engiopt/diffusion_2d_cond", "diffusion_2d_cond.py"
+    ),
 }
 
 problems = ["beams2d", "heatconduction2d", "thermoelastic2d"]
