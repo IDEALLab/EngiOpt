1398f41ed3b0dcf35d1bd8828cf7c211026add8e
Arthur Drake <adrake17@umd.edu>
2025-09-23 15:55:46 +0200

    add vqgan args and some new image transform functions

--- engiopt/transforms.py
+++ engiopt/transforms.py
@@ -1,10 +1,12 @@
 """Transformations for the data."""
 
 from collections.abc import Callable
+import math
 
 from engibench.core import Problem
 from gymnasium import spaces
 import torch as th
+import torch.nn.functional as f
 
 
 def flatten_dict_factory(problem: Problem, device: th.device) -> Callable:
@@ -21,3 +23,41 @@ def flatten_dict_factory(problem: Problem, device: th.device) -> Callable:
         return th.stack(flattened)
 
     return flatten_dict
+
+
+def _nearest_power_of_two(x: int) -> int:
+    """Round x to the nearest power of 2."""
+    lower = 2 ** math.floor(math.log2(x))
+    upper = 2 ** math.ceil(math.log2(x))
+    return upper if abs(x - upper) < abs(x - lower) else lower
+
+
+def upsample_nearest(data: th.Tensor, mode: str="bicubic") -> th.Tensor:
+    """Upsample 2D data to the nearest 2^n dimensions. Data should be a Tensor in the format (B, C, H, W)."""
+    _, _, h, w = data.shape
+    target_h = _nearest_power_of_two(h)
+    target_w = _nearest_power_of_two(w)
+    # If nearest power of two is smaller, multiply it by 2
+    if target_h < h:
+        target_h *= 2
+    if target_w < w:
+        target_w *= 2
+    return f.interpolate(data, size=(target_h, target_w), mode=mode)
+
+
+def downsample_nearest(data: th.Tensor, mode: str="bicubic") -> th.Tensor:
+    """Downsample 2D data to the nearest 2^n dimensions. Data should be a Tensor in the format (B, C, H, W)."""
+    _, _, h, w = data.shape
+    target_h = _nearest_power_of_two(h)
+    target_w = _nearest_power_of_two(w)
+    # If nearest power of two is larger, divide it by 2
+    if target_h > h:
+        target_h //= 2
+    if target_w > w:
+        target_w //= 2
+    return f.interpolate(data, size=(target_h, target_w), mode=mode)
+
+
+def resize_to(data: th.Tensor, h: int, w: int, mode: str = "bicubic") -> th.Tensor:
+    """Resize 2D data back to any desired (h, w). Data should be a Tensor in the format (B, C, H, W)."""
+    return f.interpolate(data, size=(h, w), mode=mode)
--- engiopt/vqgan/vqgan.py
+++ engiopt/vqgan/vqgan.py
@@ -1,6 +1,7 @@
+# ruff: noqa: F401 # REMOVE THIS LATER
 """Vector Quantized Generative Adversarial Network (VQGAN).
 
-Based on https://github.com/dome272/VQGAN-pytorch.
+Based on https://github.com/dome272/VQGAN-pytorch with an "Online Clustered Codebook" for better codebook usage from https://github.com/lyndonzheng/CVQ-VAE/blob/main/quantise.py
 
 VQGAN is composed of two primary Stages:
     - Stage 1 is similar to an autoencoder (AE) but with a discrete latent space represented by a codebook.
@@ -14,3 +15,127 @@ To make VQGAN conditional, we train a separate VQGAN on the conditions only (CVQ
 
 We have updated the transformer architecture, converted VQGAN from a two-stage to a single-stage approach, added several new arguments, switched to wandb for logging, added greyscale support to the perceptual loss, and more.
 """
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+import os
+import random
+import time
+
+from engibench.utils.all_problems import BUILTIN_PROBLEMS
+import matplotlib.pyplot as plt
+import numpy as np
+import torch as th
+from torch import autograd
+from torch import nn
+from torch.nn import functional
+import tqdm
+import tyro
+import wandb
+
+from engiopt.metrics import dpp_diversity
+from engiopt.metrics import mmd
+from engiopt.transforms import resize_to
+from engiopt.transforms import upsample_nearest
+
+
+@dataclass
+class Args:
+    """Command-line arguments for VQGAN."""
+
+    problem_id: str = "beams2d"
+    """Problem identifier for 2D engineering design."""
+    algo: str = os.path.basename(__file__)[: -len(".py")]
+    """The name of this algorithm."""
+
+    # Tracking
+    track: bool = True
+    """Track the experiment with wandb."""
+    wandb_project: str = "engiopt"
+    """Wandb project name."""
+    wandb_entity: str | None = None
+    """Wandb entity name."""
+    seed: int = 1
+    """Random seed."""
+    save_model: bool = False
+    """Saves the model to disk."""
+
+    # Algorithm-specific: General
+    conditional: bool = True
+    """whether the model is conditional or not"""
+
+    # Algorithm-specific: Stage 1 (AE)
+    # From original implementation: assume image_channels=1, use greyscale LPIPS only, use_Online=True, determine image_size automatically, calculate decoder_start_resolution automatically
+    n_epochs_1: int = 100
+    """number of epochs of training"""
+    batch_size_1: int = 16
+    """size of the batches"""
+    lr_1: float = 2e-4
+    """learning rate for Stage 1"""
+    beta: float = 0.25
+    """beta hyperparameter for the codebook commitment loss"""
+    b1: float = 0.5
+    """decay of first order momentum of gradient"""
+    b2: float = 0.9
+    """decay of first order momentum of gradient"""
+    n_cpu: int = 8
+    """number of cpu threads to use during batch generation"""
+    latent_dim: int = 16
+    """dimensionality of the latent space"""
+    codebook_vectors: int = 256
+    """number of vectors in the codebook"""
+    disc_start: int = 0
+    """epoch to start discriminator training"""
+    disc_factor: float = 1.0
+    """weighting factor for the adversarial loss from the discriminator"""
+    rec_loss_factor: float = 1.0
+    """weighting factor for the reconstruction loss"""
+    perceptual_loss_factor: float = 1.0
+    """weighting factor for the perceptual loss"""
+    encoder_channels: tuple = (128, 128, 128, 256, 256, 512)
+    """list of channel sizes for each encoder layer"""
+    encoder_attn_resolutions: tuple = (16,)
+    """list of resolutions at which to apply attention in the encoder"""
+    encoder_num_res_blocks: int = 2
+    """number of residual blocks per encoder layer"""
+    encoder_start_resolution: int = 256
+    """starting resolution for the encoder"""
+    decoder_channels: tuple = (512, 256, 256, 128, 128)
+    """list of channel sizes for each decoder layer"""
+    decoder_attn_resolutions: tuple = (16,)
+    """list of resolutions at which to apply attention in the decoder"""
+    decoder_num_res_blocks: int = 3
+    """number of residual blocks per decoder layer"""
+    sample_interval: int = 1600
+    """interval between image samples"""
+
+    # Algorithm-specific: Stage 1 (Conditional AE if the model is conditional)
+    cond_dim: int = 3
+    """dimensionality of the condition space"""
+    cond_hidden_dim: int = 256
+    """hidden dimension of the CVQGAN MLP"""
+    cond_latent_dim: int = 4
+    "individual code dimension for CVQGAN"
+    cond_codebook_vectors: int = 256
+    """number of vectors in the CVQGAN codebook"""
+    cond_feature_map_dim: int = 4
+    """feature map dimension for the CVQGAN encoder output"""
+
+
+    # Algorithm-specific: Stage 2 (Transformer)
+    # From original implementation: assume pkeep=1.0, sos_token=0, bias=True
+    n_epochs_2: int = 100
+    """number of epochs of training"""
+    batch_size_2: int = 16
+    """size of the batches"""
+    lr_2: float = 6e-4
+    """learning rate for Stage 2"""
+    n_layer: int = 12
+    """number of layers in the transformer"""
+    n_head: int = 12
+    """number of attention heads"""
+    n_embd: int = 768
+    """transformer embedding dimension"""
+    dropout: float = 0.3
+    """dropout rate in the transformer"""
